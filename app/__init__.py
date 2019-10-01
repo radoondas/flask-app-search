a43fc9af0d4c882a2823433c56495607812c3265
radoondas <ondas.radovan@gmail.com>
2019-10-01 15:05:30 +0200

    Fixes hardcoded config value. This enables now to configure APP_SEARCH_USE_HTTPS in .env file and correct assignement

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,28 @@ from elastic_app_search import Client
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
+def str_to_bool(s):
+    if s == 'True':
+         return True
+    elif s == 'False':
+         return False
+    else:
+        raise ValueError("Cannot covert {} to a bool".format(s))
+
+
+bool_value = str_to_bool(app.config['APP_SEARCH_USE_HTTPS'])
+
 client_app_search = Client(
     api_key=app.config['APP_SEARCH_API_KEY'],
     base_endpoint=app.config['APP_SEARCH_BASE_ENDPOINT'],
-    use_https=app.config['APP_SEARCH_USE_HTTPS']
+    use_https=bool_value
 )
 
 client_blog_search = Client(
     api_key=app.config['APP_SEARCH_API_KEY_BLOG'],
     base_endpoint=app.config['APP_SEARCH_BASE_ENDPOINT'],
-    use_https=app.config['APP_SEARCH_USE_HTTPS']
+    use_https=bool_value
 )
 
 from app import routes
--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
     APP_SEARCH_API_KEY = os.environ.get('APP_SEARCH_API_KEY')
     APP_SEARCH_API_KEY_BLOG = os.environ.get('APP_SEARCH_API_KEY_BLOG')
     APP_SEARCH_BASE_ENDPOINT = os.environ.get('APP_SEARCH_BASE_ENDPOINT') or 'localhost:3002/api/as/v1'
-    # APP_SEARCH_USE_HTTPS = bool(os.environ.get('APP_SEARCH_USE_HTTPS')) or False
-    APP_SEARCH_USE_HTTPS = False or False
+    APP_SEARCH_USE_HTTPS = os.environ.get('APP_SEARCH_USE_HTTPS') or 'False'
+    # APP_SEARCH_USE_HTTPS = False or False
     POSTS_PER_PAGE = int(os.environ.get('APP_SEARCH_DOCS_PER_PAGE')) or 10
